9664d02e0a4f353f31e7c9d3f409b80a8375dc3b
Deepu <deepaktmalai65@gmail.com>
2024-08-11 18:46:36 +0530

    Delete DemoSpringBoot directory

--- DemoSpringBoot/src/main/java/com/deepu/demospringboot/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.deepu.demospringboot;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends MongoRepository<Student,String> {
-
-}
